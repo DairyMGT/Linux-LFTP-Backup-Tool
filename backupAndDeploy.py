d923777c571a828e875639f812c146805bf0a210
nakzz <naqibprio10@gmail.com>
2018-11-25 03:36:23 -0600

    Packaging and deploy sequence implemented
    Hasn't been tested on a system yet. Might have errors.

--- backupAndDeploy.py
+++ backupAndDeploy.py
@@ -1,21 +1,107 @@
 # import packages
+import json
+import os
+from datetime import date
 
 
-# initialize variables
+#helper methods
+def executeCommand(command, filename=None):
+    print("executing: ", command)
+    # TODO: execute packaging command in terminal
 
+    if (filename !=None & os.path.exists( filename)):  # If filename parameter was passed
+        deployFiles.append(filename)    # add to deployment list
+        return True
+    else:
+        return False
 
 
 # read from config
+with open('config.json') as json_data_file:
+    configData = json.load(json_data_file)
+
+ftpHost = configData["ftp"]["host"]
+ftpUsername = configData["ftp"]["username"]
+ftpPassword = configData["ftp"]["password"]
+ftpDir = configData["ftp"]["password"]
+mysqlUsername = configData["mysql"]["backupDir"]
+mysqlPassword = configData["mysql"]["password"]
+excludeDir = configData["excludeDir"]
+includeDir = configData["includeDir"]
+deploymentDir = configData["deploymentDir"]
+backupMySql = configData["backupMySql"]
+
+# initialize variables
+command = ""
+filename = ""
+deployFiles = []
+dateFormat = str(date.today().year) + str(date.today().month) + str(date.today().day)
+readyToDeploy = True
+
+# create folders if doesn't exist
+# if((os.path.isdir(deploymentDir)) != True):
+#     os.mkdir(deploymentDir)
+#     os.mkdir(deploymentDir + "/sqlDumps")
+#     os.mkdir(deploymentDir + "/compressedDirs")
 
 
-# package directory tars
+if (deploymentDir):  # if folder exists, start packaging
+
+    # package directory tars
+    filename = deploymentDir + "compressedDirs/"+ dateFormat + "_site.tar.gz"  # format filename and command
+    command = "tar -czvf " + filename
+
+    for x in excludeDir:  # excluded directories
+        command += " --exclude=" + x
+
+    for x in includeDir:  # included directories
+        command += " " + x
+
+    if (executeCommand(command, filename)):  # Executing command
+        readyToDeploy = True
+        print("::LOG:: Directory Compression: SUCCEEDED")
+    else:
+        readyToDeploy = False
+        print("::LOG:: Directory Compression: FAILED")
+
+    # package database dumps
+    filename =  deploymentDir +"sqlDumps/" + dateFormat+ "_mysqlDump.sql"
+    command = "mysqldump -u " + mysqlUsername + " -p'" + mysqlPassword + "' --all-databases >" + deploymentDir + filename
+
+    if (executeCommand(command, filename)):  # Executing command
+        readyToDeploy = True
+        print("LOG:: MySql Dump: SUCCEEDED")
+    else:
+        readyToDeploy = False
+        print("LOG:: MySql Dump: FAILED")
+
+    # deploying to box and managing:
+    command = "lftp -u '"+ftpUsername+","+ftpPassword+"' "+ftpHost+" -e 'mkdir"+ftpDir+"; cd "+ftpDir+"; "
+    #cd /DairyMgt_Backups; put /var/boxBackupDeployment/20181107_initialMySQL.sql; '"
+
+    #add deployment files
+    if(len(deployFiles) > 0):
+        for package in deployFiles:
+            command += "put " + package + "; "
+    else:
+        print("LOG:: Deployment List is empty! Something went wrong")
 
 
+    #TODO: Remove old files
 
-# package database dumps
+    command += " '"
+    if(readyToDeploy):
+        if(executeCommand(command)):
+            print("LOG:: LFTP Deployment: SUCCEEDED")
+        else:
+            print("LOG:: LFTP Deployment: FAILED")
+    else:
+        print("LOG:: LFTP Deployment: FAILED")
 
+
+else:
+    print(deploymentDir, " does not exist nor was created. Something went wrong. Check permission and location of deployentDir in config.json")
 
 # deploy files to host
-
-
-
+if (readyToDeploy):
+    print("Deploying")
--- testFile.py
+++ testFile.py
@@ -1,9 +1,30 @@
 import json
+from datetime import date
 
 with open('config.json') as json_data_file:
     data = json.load(json_data_file)
 
-print(data['ftp'])
+print(data['includeDir'])
 
+for x in data['includeDir']:
+    print(x)
 
 print(data['ftp']['host'])
+
+dateFormat =  str(date.today().year) + str(date.today().month) + str(date.today().day)
+print(dateFormat)
+
+print(len(data['includeDir']))
+
+
+
+def f(first, second=None):
+  print(first, second)
+  if(second):
+    print(second)
+
+
+f("abc", "cbd")
+
+
+f("abc Test")
